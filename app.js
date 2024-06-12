0df9c6b8e13f010c2fa6ce41305e72ba3e00d7da
SowmyaSri22 <sowmyasri0222@gmail.com>
2024-06-12 11:33:09 +0530

    commit

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ const checkGameStatus = () => {
         disableAllBoxes();
     } else if (checkDraw()) {
         showMessage("It's a Draw!");
+        blast();
     }
 };
 
@@ -68,6 +69,7 @@ const checkWin = () => {
         if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
             if (pos1 === pos2 && pos2 === pos3) {
                 showMessage(`Congratulations, Winner is ${pos1}`);
+                blast();
                 return true;
             }
         }
@@ -147,3 +149,45 @@ playComputerBtn.addEventListener("click", playComputer);
 twoPlayerBtn.addEventListener("click", twoPlayer);
 
 updateTurnMessage();
+
+const blast=()=>{
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
